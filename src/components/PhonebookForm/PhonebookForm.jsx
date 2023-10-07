ad2839b68b130e32b93c12f2193da142878c415e
Maria Novosad <100295231+avemasha@users.noreply.github.com>
2023-10-07 17:01:53 +0200

    ready

---
+++ src/components/PhonebookForm/PhonebookForm.jsx
@@ -0,0 +1,60 @@
+import { useDispatch } from 'react-redux';
+import { addContacts } from '../store/slice';
+import { Formik } from 'formik';
+
+import { Input, Forma, Label, Button, ErMessage } from './PhonebookForm.styled';
+
+
+const formValues = {
+  name: '',
+  number: '',
+};
+
+export const PhonebookForm = () => {
+  const dispatch = useDispatch();
+  const handleSubmit = (values, { resetForm }) => {
+    dispatch(addContacts(values));
+    resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={formValues}
+     
+      onSubmit={handleSubmit}
+    >
+      <Forma autoComplete="off">
+        <Label htmlFor="name">
+          Name
+          <Input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <ErMessage name="name" component="p" />
+        </Label>
+        <Label htmlFor="number">
+          Number
+          <Input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+          <ErMessage name="number" component="p" />
+        </Label>
+        <Button type="submit">Add contact</Button>
+      </Forma>
+    </Formik>
+  );
+  
+
+  
+};
+
+
+
+
---
+++ src/components/store/slice.js
@@ -0,0 +1,68 @@
+import { createSlice, nanoid } from '@reduxjs/toolkit';
+const contactInitialState = {
+  contacts: [
+    {
+      text: { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      id: 1,
+      completed: false,
+    },
+    {
+      text: { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      id: 2,
+      completed: false,
+    },
+    {
+      text: { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      id: 3,
+      completed: false,
+    },
+    {
+      text: { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      id: 4,
+      completed: false,
+    },
+  ],
+  filter: '',
+};
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: contactInitialState,
+  reducers: {
+    addContacts: {
+      reducer(state, action) {
+        state.contacts.push(action.payload);
+      },
+      prepare(text) {
+        return {
+          payload: {
+            text,
+            id: nanoid(),
+            completed: false,
+          },
+        };
+      },
+    },
+    deleteContact(state, action) {
+      const index = state.contacts.findIndex(
+        task => task.id === action.payload
+      );
+      state.contacts.splice(index, 1);
+    },
+  },
+});
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState: contactInitialState,
+  reducers: {
+    addFilter: {
+      reducer(state, action) {
+        state.filter = action.payload;
+      },
+    },
+  },
+});
+export const { addContacts, deleteContact } = contactsSlice.actions;
+export const contactsReducer = contactsSlice.reducer;
+export const { addFilter } = filterSlice.actions;
+export const filterReducer = filterSlice.reducer;
